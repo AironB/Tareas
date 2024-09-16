346b69217db3066a22cd04cf81620cb7e01b3681
= <=>
2024-09-15 19:13:44 -0600

    primer commit

--- Alojamiento/index.php
+++ Alojamiento/index.php
@@ -2,3 +2,28 @@
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Trivago</title>
+</head>
+<body background="https://www.kayak.com.sv/rimg/himg/da/7a/64/expediav2-8100-c09b49-534863.jpg?width=1366&height=768&crop=true">
+    <div class="container">
+        <div>
+        <input type="text" class="text" placeholder="Usuario"><br>
+        </div>
+        <div>
+        <input type="text" class="text" placeholder="Password">
+        </div>
+        
+        <button type="button" class="btn btn-success">Iniciar Sesion</button>
+        <button type="button" class="btn btn-primary">Registrarse</button>
+        </div>
+    </div>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
